5533a717d5b51990bc979988608bc4d5317701cb
sazid526 <arefin15-14526@diu.edu.bd>
2023-09-14 09:07:57 +0600

    a

---
+++ covid_tracker_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:covid_tracker_app/View/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
